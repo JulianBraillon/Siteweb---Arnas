f7fae291e803981d87dbf4507f5b39b35b38d92b
JulianBraillon <jbraillon5@gmail.com>
2025-04-09 14:23:50 +0200

    Passage au php

---
+++ Accueil-Arnas.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="accueil-arnas.css">
+    <title>Accueil - Arnas</title>
+</head>
+
+<header>
+    <div class="header">
+        <img src="Images - Arnas\Logo_Arnas.svg" id="logoArnas">
+        <a href="Accueil-Arnas.php"><img src="Images - Arnas\MaisonLogo.png" class="logo">ACCUEIL</a>
+        <a href="#"><img src="Images - Arnas\maison-municipale.png" class="logo">VIE MUNICIPALE</a>
+        <a href="#"><img src="Images - Arnas\Personne-Logo.png" class="logo">VIE QUOTIDIENNE</a>
+        <a href="Demarches-Arnas.php"><img src="Images - Arnas\Fichier-Logo.png" class="logo ">DEMARCHES</a>
+    </div>  
+</header>
+
+<body>
+    <div class="top-body">
+        <div class="img-body">
+            <h2>Bienvenue à</h2>
+            <h1>ARNAS</h1>
+        </div>
+    </div>
+    <div class="middle-body">
+        <div class="middle-body-1">
+            <div class="middle-body-1-child-1">
+                <h1>Actualités</h1>
+                <div class="img-actualite">
+                    <div>
+                        <a href="#"><img src="Images - Arnas\RAM.jpg" class="img-actualite-img"></a>
+                        <div class="div-p">
+                            <h2>Inscription école maternelle</h2>
+                            <p>Le conseil municipal se réunira, le mercredi 19 mars 2025, à 20h30, en mairie. [...]</p>
+                        </div>
+                        <div class="div-button">
+                            <a href="#" id="btn-actualite">en savoir plus</a>
+                        </div>
+                    </div>
+                    <div>
+                        <a href="#"><img src="Images - Arnas\Mairie.jpg" class="img-actualite-img"></a>
+                        <div class="div-p">
+                            <h2>Journée de la citoyenneté</h2>
+                            <p>Les inscriptions en petite section de maternelle se feront, en mairie, à partir du samedi 15 février 2025. [...]</p>
+                        </div>
+                        <div class="div-button">
+                            <a href="#" id="btn-actualite">en savoir plus</a>
+                        </div>
+                    </div>
+                    <div>
+                        <a href="#"><img src="Images - Arnas\Grange Perret.jpg" id="img-etc"></a>
+                        <div class="div-p">
+                            <h2>Autre</h2>
+                            <p>Accedé aux différentes actualités grâce au bouton ci-dessous</p>
+                        </div>
+                        <div class="div-button">
+                            <a href="#" id="btn-actualite">en savoir plus</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="middle-body-1-child-2">
+                <h1>Mot du maire</h1>
+                <img src="Images - Arnas\Maire.jpg">
+                <p>"Ce site a pour vocation de vous faciliter l’accès aux informations qui vous sont utiles dans votre vie quotidienne à Arnas ou pour préparer votre séjour sur notre commune. [...]"</p>
+                <button><a href="#">lire la suite</a></button>
+            </div>
+        </div>
+        <section class="middle-body-2">
+            <div class="middle-body-2-child-1">
+                <h1>Procédés à vos <span>démarches</span> !</h1>
+                <p>Accédez facilement à toutes les démarches administratives dont vous avez besoin grâce au bouton ci-dessous !</p>
+                <a href="Demarches-Arnas.php">démarches</a>
+                <div class="icon-message-1"></div>
+                <div class="icon-message-2"></div>
+            </div>
+        </section>
+        <div class="middle-body-3">
+            salut
+        </div>
+    </div>
+</body>
+</html>
---
+++ Demarches-Arnas.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="demarches-arnas.css">
+    <title>Accueil - Arnas</title>
+</head>
+
+<header>
+    <div class="header">
+        <img src="Images - Arnas\Logo_Arnas.svg" id="logoArnas">
+        <a href="Accueil-Arnas.php"><img src="Images - Arnas\MaisonLogo.png" class="logo">ACCUEIL</a>
+        <a href="#"><img src="Images - Arnas\maison-municipale.png" class="logo">VIE MUNICIPALE</a>
+        <a href="#"><img src="Images - Arnas\Personne-Logo.png" class="logo">VIE QUOTIDIENNE</a>
+        <a href="Demarches-Arnas.php"><img src="Images - Arnas\Fichier-Logo.png" class="logo ">DEMARCHES</a>
+    </div>  
+</header>
+<body>
+    <div class="top-body">
+        <div class="img-body">
+            <h2>Procédé maintenant à vos</h2>
+            <h1>DÉMARCHES</br>ADMINISTRATIVE</h1>
+        </div>
+    </div>
+    <div class="middle-body">
+        <div class="middle-body-1">
+            <h1>
+                Démarches
+            </h1>
+            <div href="#" class="card">
+                <div class="card-child">
+                    <img src="Images - Arnas\carte-didentite.png" class="img-middle-body">
+                </div>
+                <div class="card-child">
+                    <h2>Carte d'identité</h2>
+                </div>
+                <div class="card-child">
+                    <a href="#">
+                        <img src="Images - Arnas\fleche.svg" class="fleche">
+                    </a>
+                </div>
+            </div>
+            <div href="#" class="card">
+                <div class="card-child">
+                    <img src="Images - Arnas\Passeport.png" class="img-middle-body">
+                </div>
+                <div class="card-child">
+                    <h2>Passeport</h2>
+                </div>
+                <div class="card-child">
+                    <a href="#">
+                        <img src="Images - Arnas\fleche.svg" class="fleche">
+                    </a>
+                </div>
+            </div>
+            <div href="#" class="card">
+                <div class="card-child">
+                    <img src="Images - Arnas\Carte-grise.png" class="img-middle-body">
+                </div>
+                <div class="card-child">
+                    <h2>Carte grise - Certificat d'immatriculation</h2>
+                </div>
+                <div class="card-child">
+                    <a href="#">
+                        <img src="Images - Arnas\fleche.svg" class="fleche">
+                    </a>
+                </div>
+            </div>
+            <div href="#" class="card">
+                <div class="card-child">
+                    <img src="Images - Arnas\Association.png" class="img-middle-body">
+                </div>
+                <div class="card-child">
+                    <h2>Subvention associations</h2>
+                </div>
+                <div class="card-child">
+                    <a href="#">
+                        <img src="Images - Arnas\fleche.svg" class="fleche">
+                    </a>
+                </div>
+            </div>
+            <div href="#" class="card">
+                <div class="card-child">
+                    <img src="Images - Arnas\Subvention-vélo.png" class="img-middle-body">
+                </div>
+                <div class="card-child">
+                    <h2>Subvention vélo</h2>
+                </div>
+                <div class="card-child">
+                    <a href="#">
+                        <img src="Images - Arnas\fleche.svg" class="fleche">
+                    </a>
+                </div>
+            </div>
+            <div href="#" class="card">
+                <div class="card-child">
+                    <img src="Images - Arnas\PACS.png" class="img-middle-body">
+                </div>
+                <div class="card-child">
+                    <h2>Pacte Civil de Solidarité (PACS)</h2>
+                </div>
+                <div class="card-child">
+                    <a href="#">
+                        <img src="Images - Arnas\fleche.svg" class="fleche">
+                    </a>
+                </div>
+            </div>
+            <div href="#" class="card">
+                <div class="card-child">
+                    <img src="Images - Arnas\Autorisation-sortie.png" class="img-middle-body">
+                </div>
+                <div class="card-child">
+                    <h2>Autorisation de sortie du territoire</h2>
+                </div>
+                <div class="card-child">
+                    <a href="#">
+                        <img src="Images - Arnas\fleche.svg" class="fleche">
+                    </a>
+                </div>
+            </div>
+            <div href="#" class="card">
+                <div class="card-child">
+                    <img src="Images - Arnas\avocat.png" class="img-middle-body">
+                </div>
+                <div class="card-child">
+                    <h2>Consultations avocats</h2>
+                </div>
+                <div class="card-child">
+                    <a href="#">
+                        <img src="Images - Arnas\fleche.svg" class="fleche">
+                    </a>
+                </div>
+            </div>
+            <div href="#" class="card">
+                <div class="card-child">
+                    <img src="Images - Arnas\chapiteau-de-cirque.png" class="img-middle-body">
+                </div>
+                <div class="card-child">
+                    <h2>Cirques et spectacles itinérants</h2>
+                </div>
+                <div class="card-child">
+                    <a href="#">
+                        <img src="Images - Arnas\fleche.svg" class="fleche">
+                    </a>
+                </div>
+            </div>
+            <div href="#" class="card">
+                <div class="card-child">
+                    <img src="Images - Arnas\carte-didentite.png" class="img-middle-body">
+                </div>
+                <div class="card-child">
+                    <h2>Autres démarches</h2>
+                </div>
+                <div class="card-child">
+                    <a href="#">
+                        <img src="Images - Arnas\fleche.svg" class="fleche">
+                    </a>
+                </div>
+            </div>
+
+        </div>
+        <div class="middle-body-1">
+            <h1>
+                Arrêtés
+            </h1>
+            
+        </div>
+    </div>
+</body>
