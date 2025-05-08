71fb684326862963dfe8e9647c92840bbb5cac56
JulianBraillon <jbraillon5@gmail.com>
2025-05-08 14:38:49 +0200

    V5

--- Accueil-Arnas.php
+++ Accueil-Arnas.php
@@ -83,4 +83,104 @@
         </div>
     </div>
 </body>
+<footer>
+    <div class="footer">
+        <div class="liste">
+            <div>
+                <h2>Adresse</h2>
+                    <p>
+                        <a href="https://www.google.com/maps?q=2,+Square+du+Souvenir,+69400+Arnas" target="_blank">
+                            2, Square du Souvenir<br>
+                            69 400 Arnas
+                        </a>
+                    </p>
+            </div>
+            <div>
+                <h2>Téléphone</h2>
+                <p>
+                    <a href="tel:0474650784">+33 4 74 65 07 84</a>
+                </p>
+            </div>
+            <div>
+                <h2>Nous Contacter</h2>
+                <a href="#">Contact</a>
+            </div>
+        </div>
+        <div>  
+            <div class="map-container">
+            <iframe
+                src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d5520.221336410911!2d4.70671!3d46.02447!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47f4f8e1eb9df325%3A0xb685cd997c9787a7!2s%C3%89glise%20Saint-Saturnin!5e0!3m2!1sfr!2sfr!4v1713274284309!5m2!1sfr!2sfr"
+                width="100%"
+                height="600"
+                style="border:0;"
+                allowfullscreen=""
+                loading="lazy"
+                referrerpolicy="no-referrer-when-downgrade">
+            </iframe>
+
+            </div>
+        </div>
+        <div class="table">
+            <table>
+                <tr class="table-titre">
+                    <th>
+                        Horaire
+                    </th>
+                </tr>
+                <tr>
+                    <th></th>
+                    <th>Matin</th>
+                    <th>Après-midi</th>
+                </tr>
+                <tr>
+                    <th>Lundi</th>
+                    <td>9h - 12h30</td>
+                    <td>14h - 17h30</td>
+                </tr>
+                <tr>
+                    <th>Mardi</th>
+                    <td>Fermé</td>
+                    <td>14h - 17h30</td>
+                </tr>
+                <tr>
+                    <th>Mercredi</th>
+                    <td>9h - 12h30</td>
+                    <td>14h - 17h30</td>
+                </tr>
+                <tr>
+                    <th>Jeudi</th>
+                    <td>9h - 12h30</td>
+                    <td>14h - 17h30</td>
+                </tr>
+                <tr>
+                    <th>Vendredi</th>
+                    <td>8h - 12h30</td>
+                    <td>14h - 16h30</td>
+                </tr>
+                <tr>
+                    <th>Samedi</th>
+                    <td>9h - 12h30</td>
+                    <td>14h - 17h30</td>
+                </tr>
+            </table>
+        </div>
+    </div>
+    <div class="bottom-footer">
+        <div class="bottom-footer-child">
+            <div><span>Pratique :</span>
+                <div><a href="">Agenda</a></div>
+                <div><a href="">Actualités</a></div>
+                <div><a href="">Mot du maire</a></div>
+                <div><a href="">Photos</a></div>
+                <div><a href="">Vidéos</a></div>
+                <div><a href="">Météo</a></div>
+            </div>
+            <div><span>Divers :</span>
+                <div><a href="#">Mentions légales</a></div>
+                <div><a href="#">Politique de confidentialité</a></div>
+                <div><a href="#">Gestion des cookies</a></div>
+            </div>
+        <div>
+    </div>
+</footer>
 </html>
